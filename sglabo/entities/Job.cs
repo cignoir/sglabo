0808972de1463a2b8bc03790760554ad33437b94
cignoir <cignoir@gmail.com>
2014-03-25 16:24:19 +0900

    AIインスタンス生成処理実装

--- sglabo/entities/Job.cs
+++ sglabo/entities/Job.cs
@@ -9,7 +9,7 @@ namespace sglabo.entities
 {
     enum Job
     {
-        戦士, 騎士, 格闘士, 幻闘士, 盗賊, 忍者, 精霊, 召喚, 黒印, 錬金, 守護, 次元
+        戦士, 騎士, 格闘, 幻闘, 盗賊, 忍者, 精霊, 召喚, 黒印, 錬金, 守護, 次元
     }
 
     class JobConverter
@@ -26,10 +26,10 @@ namespace sglabo.entities
                     job = Job.騎士;
                     break;
                 case "格闘士":
-                    job = Job.格闘士;
+                    job = Job.格闘;
                     break;
                 case "幻闘士":
-                    job = Job.幻闘士;
+                    job = Job.幻闘;
                     break;
                 case "盗賊":
                     job = Job.盗賊;
@@ -71,28 +71,40 @@ namespace sglabo.entities
                     ai = new Warrior();
                     break;
                 case "騎士":
+                    ai = new Warrior();
                     break;
-                case "格闘士":
+                case "格闘":
+                    ai = new Monk();
                     break;
-                case "幻闘士":
+                case "幻闘":
+                    ai = new Monk();
                     break;
                 case "盗賊":
+                    ai = new Thief();
                     break;
                 case "忍者":
+                    ai = new Thief();
                     break;
                 case "精霊":
+                    ai = new SpiritMage();
                     break;
                 case "召喚":
+                    ai = new SpiritMage();
                     break;
                 case "黒印":
+                    ai = new SealMage();
                     break;
                 case "錬金":
+                    ai = new SealMage();
                     break;
                 case "守護":
+                    ai = new ProtectionMage();
                     break;
                 case "次元":
+                    ai = new ProtectionMage();
                     break;
                 default:
+                    ai = new Warrior();
                     break;
             }
             return ai;
